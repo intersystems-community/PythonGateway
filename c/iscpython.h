e55a4a02b020b0538d5d85dc8d81d83429c496f1
eduard93 <lebeduk93@gmail.com>
2018-12-24 23:08:29 +0300

    Add header file

---
+++ c/iscpython.h
@@ -0,0 +1,25 @@
+/*
+ *  iscpython.h
+ *
+ *  Created on: 16 Dec 2018
+ *      Author: Eduard
+ */
+
+#ifndef ISCPYTHON_H
+#define ISCPYTHON_H
+
+
+// Production methods
+int Initialize();
+int Finalize();
+int SimpleString(char *command, char *resultVar, CACHE_EXSTRP result);
+
+// Test methods
+int GetRandom(double* random);
+int GetRandomSimple(double* random);
+int SimpleStringFull(char *command, double* result);
+
+// Console test method
+int main(int argc, char **argv);
+
+#endif /* ISCPYTHON_H */
