0bc158f514a93a4c8cbcbf8ca15f7b64e94f892d
eduard93 <lebeduk93@gmail.com>
2020-01-15 23:27:24 +0300

    Jupyter: process creation/renaming/deletion. Checkpoints proxy.

--- jupyter/MLContentsManager.py
+++ jupyter/MLContentsManager.py
@@ -1,7 +1,7 @@
 import irisnative
 from notebook.services.contents.manager import ContentsManager
 import json
-from traitlets import Unicode, Integer, HasTraits
+from traitlets import Unicode, Integer, HasTraits, default
 
 class MLContentsManager(ContentsManager, HasTraits):
 
@@ -35,6 +35,11 @@ class MLContentsManager(ContentsManager, HasTraits):
         help='InterSystems IRIS Password',
     )
 
+
+    @default('checkpoints_class')
+    def _checkpoints_class_default(self):
+        return MLCheckpoints
+
     className = 'isc.py.util.Jupyter'
 
     def __init__(self, **kwargs):
@@ -65,26 +70,67 @@ class MLContentsManager(ContentsManager, HasTraits):
         '''
         Delete the file or directory at path.
         '''
-        return True
+        return self.iris.classMethodValue(self.className, 'Delete', path)
 
     def rename_file(self, old_path, new_path):
         '''
         Rename a file or directory.
         '''
-        return True
+        return self.iris.classMethodValue(self.className, 'Rename', old_path, new_path)
 
     def file_exists(self, path):
         '''
         Does a file exist at the given path?
         '''
-        return True
+        return self.iris.classMethodValue(self.className, 'Exists', path)
 
     def dir_exists(self, path):
         '''
         Does a directory exist at the given path?
         '''
-        return True
+        return self.iris.classMethodValue(self.className, 'ExistsDir', path)
 
     def is_hidden(self, path):
         return False
 
+from notebook.services.contents.checkpoints import (
+    Checkpoints,
+    GenericCheckpointsMixin,
+)
+
+class MLCheckpoints(GenericCheckpointsMixin, Checkpoints):
+    className = 'isc.py.util.JupyterCheckpoints'
+
+
+    """requires the following methods:"""
+    def create_file_checkpoint(self, content, format, path):
+        """ -> checkpoint model"""
+        return self.parent.iris.classMethodValue(self.className, 'CreateFile',  json.dumps(content), format, path)
+
+
+    def create_notebook_checkpoint(self, nb, path):
+        """ -> checkpoint model"""
+        return self.parent.iris.classMethodValue(self.className, 'CreateNotebook', json.dumps(nb), path)
+
+    def get_file_checkpoint(self, checkpoint_id, path):
+        """ -> {'type': 'file', 'content': <str>, 'format': {'text', 'base64'}}"""
+        #return self.parent.iris.classMethodValue(self.className, 'CreateNotebook', nb, path)
+        return self.parent.get(path, True, None)
+
+    def get_notebook_checkpoint(self, checkpoint_id, path):
+        """ -> {'type': 'notebook', 'content': <output of nbformat.read>}"""
+        return self.parent.get(path, True, 'text')
+
+    def delete_checkpoint(self, checkpoint_id, path):
+        """deletes a checkpoint for a file"""
+        return self.parent.iris.classMethodValue(self.className, 'DeleteCheckpoint', checkpoint_id, path)
+
+    def list_checkpoints(self, path):
+        """returns a list of checkpoint models for a given file,
+        default just does one per file
+        """
+        return []
+
+    def rename_checkpoint(self, checkpoint_id, old_path, new_path):
+        """renames checkpoint from old path to new path"""
+        return self.parent.iris.classMethodValue(self.className, 'RenameCheckpoint', checkpoint_id, old_path, new_path)
